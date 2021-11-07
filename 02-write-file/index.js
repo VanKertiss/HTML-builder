78a0ab69bdf69872491ff8a9c822b9b6433dd041
VanKertiss <56919897+VanKertiss@users.noreply.github.com>
2021-11-07 13:41:18 +0300

    feat: add write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+let writableStream = fs.createWriteStream(
+  './02-write-file/data.txt'
+);
+
+const write = (text) => {
+  fs.appendFile('./02-write-file/data.txt', text, function (error) {
+    if (error) throw error; // если возникла ошибка
+    if (text === 'exit') {
+      console.log(text);
+      return readline.close();
+    }
+    startWriteFile();
+  });
+};
+
+const startWriteFile = () => {
+  readline.question('add your text ', (text) => {
+
+    write(text);
+  });
+};
+
+startWriteFile();
+
+writableStream.on('error', (err) => console.log(err));
